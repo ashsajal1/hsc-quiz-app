de584831712932015e5131cda704fd5c87a3f6e6
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-11-26 18:04:34 +0600

    feat: update improt

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-// Create a single supabase client for interacting with your database
-export const supabase = createClient(import.meta.env.SUPABASE_URL!, import.meta.env.SUPABASE_KEY!);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
