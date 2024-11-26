7abbacb53293f316c0433d6b6d862c1332823d01
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-11-26 08:18:00 +0600

    feat: add quiz page

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import { createWebHistory, createRouter } from "vue-router";
 import HomeView from "./pages/Home.vue";
 import AboutView from "./pages/About.vue";
 import NotFound from "./pages/NotFound.vue";
+import Quiz from "./pages/Quiz.vue";
 
 const routes = [
   { path: "/", component: HomeView },
@@ -12,6 +13,14 @@ const routes = [
     meta: {
       title: "About Us",
       description: "Learn more about us on this page.",
+    },
+  },
+  {
+    path: "/quiz",
+    component: Quiz,
+    meta: {
+      title: "Play Quizzes",
+      description: "Play quizzes, practice and get golden a plus.",
     },
   },
   { path: "/:pathMatch(.*)*", component: NotFound },
