26caaf17e00a1198e654f09581ff017e09f023d5
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-11-26 09:18:18 +0600

    feat: udpate id

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -4,7 +4,7 @@ import { Client, Account, OAuthProvider } from 'appwrite'
 const client = new Client()
 client
   .setEndpoint('https://cloud.appwrite.io/v1')// The Appwrite API endpoint
-  .setProject('674304b2002f919b31ea')// Your Appwrite project IDexport const account = new Account(client)
+  .setProject('67453c5d000e7040a66e')// Your Appwrite project IDexport const account = new Account(client)
 export { OAuthProvider }
 export const account = new Account(client);
 
