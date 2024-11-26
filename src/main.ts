7a9ded7a8aa4f8f27f1805567997cd62c6a1c0dd
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-11-26 08:35:33 +0600

    feat: setup pinia

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import App from './App.vue'
 import { router } from './router'
 import { MotionPlugin } from '@vueuse/motion'
 import PrimeVue from 'primevue/config';
+import { createPinia } from 'pinia';
+const pinia = createPinia();
+const app = createApp(App);
 
-const app = createApp(App)
-
-app.use(router);
+app.use(router); 
+app.use(pinia); 
 app.use(MotionPlugin);
 app.use(PrimeVue, {
     theme: 'none'
